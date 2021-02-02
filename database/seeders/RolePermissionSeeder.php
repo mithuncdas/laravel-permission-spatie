1e0371facc78b029afef2f3661a041223e8d5af2
mithundas404 <mithuncse404@gmail.com>
2021-02-02 11:38:32 +0600

    seed complete

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Create Roles
+        $roleSuperAdmin = Role::create(['name' => 'superadmin']);
+        $roleAdmin = Role::create(['name' => 'admin']);
+        $roleEditor = Role::create(['name' => 'editor']);
+        $roleUser = Role::create(['name' => 'user']);
+        //Permission Lis as array
+
+        $permissions = [
+
+            //dashboard
+            'dashboard.view',
+
+
+            //admin permissions
+            'blog.create',
+            'blog.view',
+            'blog.edit',
+            'blog.delete',
+            'blog.approve',
+
+            //admin permissions
+            'admin.create',
+            'admin.view',
+            'admin.edit',
+            'admin.delete',
+            'admin.approve',
+
+            //roll  permissions
+            'role.create',
+            'role.view',
+            'role.edit',
+            'role.delete',
+            'role.approve',
+
+            //profile  permissions
+            'profile.create',
+            'profile.view',
+            'profile.edit',
+            'profile.delete',
+            'profile.approve',
+        ];
+
+        //Create persmiison and Assign Permission
+        // $permission = Permission::create(['name' => 'edit articles']);
+
+        for ($i=0; $i < count($permissions); $i++) {
+            // create permission
+             $permission = Permission::create(['name' => $permissions[$i]]);
+             $roleSuperAdmin->givePermissionTo($permission);
+             $permission->assignRole($roleSuperAdmin);
+
+        }
+    }
+}
